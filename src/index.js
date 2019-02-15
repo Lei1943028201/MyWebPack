42bf140435d167dbd4986a26d589d7a873d62b5b
Lei1943028201 <lei1248421>
2019-02-15 14:29:44 +0800

    加载图片资源

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import _ from 'lodash';
-import './style.css'
+import './style.css';
+import Icon from'./begin.png';
 function component() {
     var element = document.createElement('div');
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello')
+    // 将图片添加到现有的div中
+    var myIcon = new Image()
+    myIcon.src = Icon
+    element.appendChild(myIcon)
     return element;
 }
 
